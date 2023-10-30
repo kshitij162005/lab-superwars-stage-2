67ee1aa90d335af8d1caa672dbf56903a704b8d1
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2023-10-30 13:21:49 +0530

    done

--- src/app.js
+++ src/app.js
@@ -26,7 +26,15 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for (let i = 0; i < players.length; i++) {
+        detailedPlayers.push({
+            name:players[i],
+            strength:getRandomStrength(),
+            image: `images/super-${i + 1}.png`,
+            type: i%2 === 0 ?'hero':'villian',
+        }
+        );
+         }
     return detailedPlayers;
 }
 
@@ -34,7 +42,9 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
-}
+        return Math.round(Math.random()*100-1)
+    }
+
 
 const buildPlayers = (players, type) => {
     let fragment = '';
@@ -42,6 +52,16 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for(let i=0 ; players.length>i ; i++){
+        if (players[i].type=== type){
+            fragment+= `
+            <div class="player">
+            <img src="${players[i].image}" alt="">
+            <div class="name">${players[i].name}</div>
+            <div class="strength">${players[i].strength}</div>
+         </div>`
+        }
+    }
 
     return fragment;
 }
